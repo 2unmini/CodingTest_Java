d4530b14d9fb0b7a929bda579ab3e67a16f34779
2unmini <2unmin4030@gmail.com>
2024-10-28 08:59:16 +0900

    [level 1] Title: 평균 구하기, Time: 0.03 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,9 +1,6 @@
 class Solution {
     public double solution(int[] arr) {
         double answer = 0;
-        for(int e:arr){
-            answer+=e;
-        }
-        return answer/arr.length;
+        return answer;
     }
 }
