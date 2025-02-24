f79905897261fd334f3788b6583320c1ddc36174
2unmini <2unmin4030@gmail.com>
2025-02-25 05:24:41 +0900

    Title: 2021년 날짜의 유무, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250225/2021년 날짜의 유무/with-or-without-2021.java
+++ 250225/2021년 날짜의 유무/with-or-without-2021.java
@@ -5,33 +5,35 @@ public class Main {
         int m = sc.nextInt();
         int d = sc.nextInt();
         // Please Write your code.
-        invalid(m,d);
+        if(invalid(m,d)){
+            System.out.print("Yes");
+        }else{
+            System.out.print("No");
+        }
     }
 
-    public static void invalid(int m,int d){
+    public static boolean invalid(int m,int d){
         switch(m){
             case 1: case 3:case 5: case 7:case 8: case 10: case 12 : {
                 if(d>31){
-                    System.out.println("No");
-                    break;
+                    return false;
+            
                 }
             }
             case 4 :case 6 : case 9 :case 11:{
                 if(d>30){
-                    System.out.println("No");
-                    break;
+                    return false;
+                   
                 }
             }
             case 2:{
                 if(d>28){
-                    System.out.println("No");
-                    break;
+                    return false;
+                    
                 }
             }
             default:
-            System.out.println("Yes");
-             break;
-
+                return true;
 
         }
     }
