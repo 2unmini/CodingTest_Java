b84b02bfa43438dbbf597b76eed90504b9518ceb
2unmini <2unmin4030@gmail.com>
2025-02-25 05:26:53 +0900

    Title: 2021년 날짜의 유무, Time: 106ms, Memory: 10MB, Status: Passed - CodeTree

--- 250225/2021년 날짜의 유무/with-or-without-2021.java
+++ 250225/2021년 날짜의 유무/with-or-without-2021.java
@@ -15,25 +15,16 @@ public class Main {
     public static boolean invalid(int m,int d){
         switch(m){
             case 1: case 3:case 5: case 7:case 8: case 10: case 12 : {
-                if(d>31){
-                    return false;
-            
-                }
+                return d >= 1 && d <= 31;
             }
             case 4 :case 6 : case 9 :case 11:{
-                if(d>30){
-                    return false;
-                   
-                }
+                 return d >= 1 && d <= 30;
             }
             case 2:{
-                if(d>28){
-                    return false;
-                    
-                }
+                return d >= 1 && d <= 28;
             }
             default:
-                return true;
+                return false;
 
         }
     }
