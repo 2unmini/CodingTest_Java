425b1ae0bca176a002cd349d58e0c39ae3732d3e
2unmini <2unmin4030@gmail.com>
2025-02-26 05:22:35 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 148ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 250226/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please Write your code.
+        print(n);
+    }
+    public static void print(int n) {
+        if(n==0){
+            return;
+        }
+        for(int i=n;i>0;i--) {
+            System.out.print("* ");
+        }
+        System.out.println();
+        print(n-1);
+
+        for(int i=0;i<n;i++) {
+            System.out.print("* ");
+        }
+        System.out.println();
+
+    }
+}
