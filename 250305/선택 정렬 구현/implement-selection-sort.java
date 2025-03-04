d7b9d97007376d6d4bed7a52f3c82278646b3512
2unmini <2unmin4030@gmail.com>
2025-03-05 05:47:32 +0900

    Title: 선택 정렬 구현, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250305/선택 정렬 구현/implement-selection-sort.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        // Please write your code here.
+        selectionSort(arr);
+    }
+    public static void selectionSort(int[] arr) {
+        for(int i =0;i<arr.length-1;i++){
+            int minIndex=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[minIndex]){
+                    minIndex=j;
+                }
+            }
+            int temp =arr[i];
+                arr[i]=arr[minIndex];
+                arr[minIndex]=temp;
+        }
+        for(int a:arr){
+            System.out.print(a+" ");
+        }
+    }
+}
